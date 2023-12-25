c902e291e4f22cab57a38e04725e96df39af6ca5
Danish Rana <65905307+DanoshRana062@users.noreply.github.com>
2023-12-25 21:04:53 +0500

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,33 +1,43 @@
-let homeNavButton=document.querySelector("#navigationButton")
-console.log(homeNavButton)
-homeNavButton.addEventListener("click",homePageSwitch)
-function homePageSwitch(event){
-    console.log(event)
-     let ok =document.getElementById("homePage")
-     let hp = document.getElementById("highlightsPage")
-    ok.classList.add("switchPage")
-    setTimeout(() => {
-        ok.style.display="none"
-        hp.style.display="grid";
-        hp.classList.add("highlightsPageFade")
-    }, 1000);  
-}
 
+let homeButton=document.querySelector("#navigationButton")
 let backbutton=document.getElementById("backNavigationButton")
+let homePage = document.getElementById("homePage");
+let highlightsPage = document.getElementById("highlightsPage");
+
 backbutton.addEventListener("click",backPage)
+homeButton.addEventListener("click",homePageSwitch)
+
+
+
+function homePageSwitch(event) {
+    homePage.classList.add("switchPage")
+
+    setTimeout(() => {
+        homePage.classList.remove("switchPage")
+        homePage.style.display = "none"
+        highlightsPage.style.display = "grid"
+
+        highlightsPage.classList.add("highlightsPageFade")
+    
+    }, 1000)
+    // 1000
+}
 
 function backPage(event) {
-    console.log(event);
-    let ok = document.getElementById("homePage");
-    let hp = document.getElementById("highlightsPage");
-    hp.classList.add("highlightsPageFadeOut");
+    highlightsPage.classList.remove("highlightsPageFade")
+
+    highlightsPage.classList.add("highlightsPageFadeOut")
     setTimeout(() => {
-        hp.style.display = "none";
-    }, 500); // Adjust timing for hiding 'highlightsPage'
-    setTimeout(() => {
-        ok.classList.add("backSwitchPage");
-        ok.style.display = "grid";
-    }, 600); // Adjust timing for showing 'homePage'
+        highlightsPage.classList.remove("highlightsPageFadeOut")
+        highlightsPage.style.display = "none"
+        homePage.style.display = "grid"
+        homePage.classList.add("backSwitchPage")
+        setTimeout(()=> {
+        homePage.classList.remove("backSwitchPage")
+
+        }, 1500)
+
+    }, 500)
+
+    // 500
 }
-
-
