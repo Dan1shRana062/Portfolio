fd81af941fbee1c0bb01003559bd4b147dd3da43
Danish Rana <65905307+Dan1shRana062@users.noreply.github.com>
2023-12-26 19:07:17 +0500

    Update typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -27,7 +27,7 @@ function typeWriter(initialText, id, speed) {
 
 typeWriter("Hi,", "greetingText", 100).then(
 
-  () => typeWriter("Danish Here.", "nameText", 100).then(() => typeWriter("Just An Average CS Student", "para", 50).then(() => typeWriter("Highlights", "navigationText", 100).then(() => {
+  () => typeWriter("Danish Here.", "nameText", 100).then(() => typeWriter("Just A CS Student. My Goal? To Dive Into The Tech World Headfirst,learn heaps,and be part of creating cool and innovative stuff.", "para", 50).then(() => typeWriter("Highlights", "navigationText", 100).then(() => {
     document.getElementById("navigationIcon").style.display = "inline"}).then(()=> {
   })))
 )
